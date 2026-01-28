d0e73a3b10eccabc0146e754907f32008d4fbc05
Niraj Kumar <your-github-email@example.com>
2026-01-28 10:55:33 -0500

    commit

--- terraform/service.tf
+++ terraform/service.tf
@@ -1,24 +1,6 @@
 resource "kubernetes_service" "app" {
   metadata {
-    name = "votingapp-service"
-  }
-
-  spec {
-    selector = {
-      app = "votingapp"
-    }
-
-    port {
-      port        = 80
-      target_port = 5000
-    }
-
-    type = "ClusterIP"
-  }
-}
-resource "kubernetes_service" "app" {
-  metadata {
-    name = "votingapp-service"
+    name = "votingapp"
   }
 
   spec {
